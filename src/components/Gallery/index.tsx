a6be4be74217970c66a290e15f3e7509b256360e
Rodrigo Cinelli <rodcinelli@gmail.com>
2024-08-18 12:02:33 -0300

    Cria a galeria parte 2 (metade)

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,26 +1,60 @@
 import Section from '../Section'
 
-import { Item, Items } from './styles'
+import { Item, Items, Action } from './styles'
 
-import zelda from '../../assets/images/zelda.png'
+import spiderman from '../../assets/images/banner-homem-aranha.png'
+import hogwarts from '../../assets/images/fundo_hogwarts.png'
 
-const Gallery = () => (
-  <Section title="Galeria" background="black">
-    <Items>
-      <Item>
-        <img src={zelda} alt="imagem do link" />
-      </Item>
-      <Item>
-        <img src={zelda} alt="imagem do link" />
-      </Item>
-      <Item>
-        <img src={zelda} alt="imagem do link" />
-      </Item>
-      <Item>
-        <img src={zelda} alt="imagem do link" />
-      </Item>
-    </Items>
-  </Section>
-)
+import play from '../../assets/images/play.png'
+import zoom from '../../assets/images/zoom.png'
+
+type GalleryItem = {
+  type: 'image' | 'video'
+  url: string
+}
+
+const mock: GalleryItem[] = [
+  {
+    type: 'image',
+    url: spiderman
+  },
+  {
+    type: 'image',
+    url: hogwarts
+  },
+  {
+    type: 'video',
+    url: 'https://www.youtube.com/embed/uHGShqcAH1Q'
+  }
+]
+
+type Props = {
+  defaultCover: string
+}
+
+const Gallery = ({ defaultCover }: Props) => {
+  const getMediaCover = (item: GalleryItem) => {
+    if (item.type === 'image') return item.url
+    return defaultCover
+  }
+
+  return (
+    <Section title="Galeria" background="black">
+      <Items>
+        {mock.map((media, index) => (
+          <Item key={media.url}>
+            <img
+              src={getMediaCover(media)}
+              alt={`Mídia ${index + 1} de Nome do jogo`}
+            />
+            <Action>
+              <img src={zoom} alt="Clique para maximizar a imagem" />
+            </Action>
+          </Item>
+        ))}
+      </Items>
+    </Section>
+  )
+}
 
 export default Gallery
