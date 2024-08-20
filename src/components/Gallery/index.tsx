6ff49032b8af9eb20803a174a853ed23580fe04d
Rodrigo Cinelli <rodcinelli@gmail.com>
2024-08-20 18:36:18 -0300

    Integrando com a API (metade)

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 import Section from '../Section'
+import { GalleryItem } from '../../Pages/Home'
 
 import { Item, Items, Action, Modal, ModalContent } from './styles'
 
@@ -10,11 +11,6 @@ import hogwarts from '../../assets/images/fundo_hogwarts.png'
 import play from '../../assets/images/play.png'
 import zoom from '../../assets/images/zoom.png'
 import fechar from '../../assets/images/fechar.png'
-
-interface GalleryItem {
-  type: 'image' | 'video'
-  url: string
-}
 
 const mock: GalleryItem[] = [
   {
--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -1,4 +1,4 @@
-import Game from '../../Models/Game'
+import { Game } from '../../Pages/Home'
 import Product from '../Product'
 
 import { Container, List } from './styles'
@@ -9,25 +9,39 @@ export type Props = {
   games: Game[]
 }
 
-const ProductsList = ({ background, title, games }: Props) => (
-  <Container background={background}>
-    <div className="container">
-      <h2>{title}</h2>
-      <List>
-        {games.map((game) => (
-          <Product
-            key={game.id}
-            category={game.category}
-            description={game.description}
-            image={game.image}
-            infos={game.infos}
-            system={game.system}
-            title={game.title}
-          />
-        ))}
-      </List>
-    </div>
-  </Container>
-)
+const ProductsList = ({ background, title, games }: Props) => {
+  const getGameTags = (game: Game) => {
+    const tags = []
+
+    if (game.release_date) {
+      tags.push(game.release_date)
+    }
+
+    if (game.prices.discount) {
+      tags.push(`${game.prices.discount}%`)
+    }
+  }
+
+  return (
+    <Container background={background}>
+      <div className="container">
+        <h2>{title}</h2>
+        <List>
+          {games.map((game) => (
+            <Product
+              key={game.id}
+              category={game.details.category}
+              description={game.description}
+              image={game.media.thumbnail}
+              infos={game.infos}
+              system={game.system}
+              title={game.title}
+            />
+          ))}
+        </List>
+      </div>
+    </Container>
+  )
+}
 
 export default ProductsList
