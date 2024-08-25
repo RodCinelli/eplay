cca91fc85170592156fc34dca8acb98307d96047
Rodrigo Cinelli <rodcinelli@gmail.com>
2024-08-24 21:16:56 -0300

    Criando o carrinho (metade)

---
+++ src/components/Cart/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+import { cores } from '../../styles'
+
+export const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #000;
+  opacity: 0.7;
+`
+
+export const CartContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-end;
+  z-index: 1;
+`
+
+export const Sidebar = styled.aside`
+  background-color: ${cores.cinza};
+  z-index: 1;
+  padding: 40px 16px 0 16px;
+`
